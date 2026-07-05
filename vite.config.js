e6f88ca896a02714cd44bbb77db730625aa15cee
Bhumika Saxena <bhumikasaxena4590@gmail.com>
2026-07-05 18:48:26 +0530

    Configure portfolio for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/MyPortfolio/',
   plugins: [react(), tailwindcss()],
 })
